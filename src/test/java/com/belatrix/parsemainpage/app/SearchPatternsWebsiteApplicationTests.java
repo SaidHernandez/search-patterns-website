9edc373e1227ec9f274875a727ade1dbf8cce1e1
SaidHernandez <said.ingsistemas@gmail.com>
2019-07-14 20:30:10 -0500

    Update Test
    Se agregan algunos casos de pruebas para garantizar el proceso

--- src/test/java/com/belatrix/parsemainpage/app/SearchPatternsWebsiteApplicationTests.java
+++ src/test/java/com/belatrix/parsemainpage/app/SearchPatternsWebsiteApplicationTests.java
@@ -1,16 +1,47 @@
 package com.belatrix.parsemainpage.app;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.belatrix.parsemainpage.app.business.ISearchPartternsBusiness;
+import com.belatrix.parsemainpage.app.processor.IFileListWebSiteProcessor;
+import com.belatrix.parsemainpage.app.processor.ISearchPartternsProcessor;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SearchPatternsWebsiteApplicationTests {
+	
+	@Autowired
+	IFileListWebSiteProcessor fileListWebSiteProcessor;
+	
+	@Autowired
+	@Qualifier("partternsHashTag")
+	ISearchPartternsProcessor searchPatternsProcessor; 
 
 	@Test
 	public void contextLoads() {
+		fileListWebSiteProcessor.readFileLocal();
+		
+	}
+	
+	@Test
+	public void readFileTest() {
+		assertNotEquals("Error al leer la lista de WebSite", 0, fileListWebSiteProcessor.readFileLocal().size());
+	}
+	
+	@Test
+	public void searchPatternsTest() {
+		List<String> listTest = fileListWebSiteProcessor.readFileLocal();
+		assertNotEquals("Error al buscar hashtag", 0, searchPatternsProcessor.findPatterns(listTest).size());
 	}
 
 }
